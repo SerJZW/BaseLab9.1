f8d17ef6951583a287401934918c28467edcc63c
ZemtsovSD <ZemtsovSD@310D-11>
2022-10-22 12:20:23 +0700

    Добавьте файлы проекта.

---
+++ BaseLab9.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace VerySimpleAuth
+{
+    class Program
+    {
+
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Пройдите регистрацию");
+                Console.Write("Введите логин: ");
+                String login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                String pass = Console.ReadLine();
+
+
+                if (login.Length > 9)
+                {
+                    Console.WriteLine("Недопустимое количество символов в логине - нужно менее 9!");
+                }
+                if (login.Length < 3)
+                {
+                    Console.WriteLine("Недопустимое количество символов в логине - нужно больше 3!");
+                }
+                if (pass.Length <3)
+                {
+                    Console.WriteLine("Недопустимое количество символов в пароле - нужно больше 3!");
+                }
+                if (pass.Length > 9)
+                {
+                    Console.WriteLine("Недопустимое количество символов в пароле - нужно меньше 9!");
+                }
+                
+                if (login == pass)
+                {
+                    Console.WriteLine("Логин и пароль совпадают, измените данные");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Регистрация прошла успешно!");
+
+                }
+                break;
+
+            }
+        }
+    }
+
+}
+
